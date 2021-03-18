ea3b8c308d044988ca749b84972acd475731f7d8
carlo carbonell <carlo1678@hotmail.com>
2021-03-18 11:12:39 -0400

    initial character classes

---
+++ charClasses.py
@@ -0,0 +1,32 @@
+# Build out Warrior, Mage, Knight, and Warlock classes
+# 100 hp, 50 strength, 50 defense, 50 magic
+# Hello Joe
+class Warrior:
+    def __init__(self):
+        self.hp = 100
+        self.strength = 50
+        self.defense = 50
+        self.magic = 50
+
+class Mage:
+    def __init__(self):
+        self.hp = 100
+        self.strength = 50
+        self.defense = 50
+        self.magic = 50
+    
+class Knight:
+    def __init__(self):
+        self.hp = 100
+        self.strength = 50
+        self.defense = 50
+        self.magic = 50
+
+class Warlock:
+    def __init__(self):
+        self.hp = 100
+        self.strength = 50
+        self.defense = 50
+        self.magic = 50
+
+
