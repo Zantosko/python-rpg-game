bc4e578ff3a719b12fbf101d0e459dd2966297ed
Tatien Miller <tatmil99@gmail.com>
2021-03-17 22:51:44 -0500

    creates a weapons class that prints out weapon specs

--- evil_char_class.py
+++ evil_char_class.py
@@ -15,3 +15,13 @@ first_goblin = Goblin(15, 8, 10, 6)
 first_goblin.printFunc()
 
 
+class Weapons:
+  def __init__(self, durability, damage):
+    self.durability = durability
+    self.damage = damage
+  def sword(self):
+    print(f"Your sword deals a damage of {self.damage}")
+    print(f"Your sword has a durability rating of {self.durability}")
+
+weathered_sword = Weapons(10, 8)
+weathered_sword.sword()
