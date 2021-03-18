866db58ba5812b7449079397f923f6b64260a739
Tatien Miller <tatmil99@gmail.com>
2021-03-18 18:51:53 -0500

    creates two weapon classes

--- evil_char_class.py
+++ evil_char_class.py
@@ -1,27 +1,29 @@
-class Goblin:
-  def __init__(self, health, damage, strength, defense):
-    self.health = health
+
+class Sword:
+  def __init__(self, damage, durability):
     self.damage = damage
-    self.strength = strength
-    self.defense = defense
-  def printFunc(self):
-    print(f"Your health level is: {self.health}")
-    print(f"Your damage level is: {self.damage}")
-    print(f"Your strength level is: {self.strength}")
-    print(f"Your defense level is: {self.defense}")
+    self.durability = durability
+    # self.hp += 5
+
+  def short_sword(self):
+    self.damage = damage
+    self.durability = durability
+  def long_sword(self, damage, durability):
+    pass
+      
+class Bow:
+  def __init__(self, damage, durability):
+    self.damage = damage
+    self.durability = durability
+  def flamming_arrow(self):
+    self.damage += 5
+    print(f"{self.damage}")
+    self.durability += 5
+    print(f"{self.durability}")
     
 
-first_goblin = Goblin(15, 8, 10, 6)
-first_goblin.printFunc()
+sword = Sword(21, 19)
+print(f"Your short sword damage and durability are {sword.damage} {sword.durability} respectively")
 
-
-class Weapons:
-  def __init__(self, durability, damage):
-    self.durability = durability 
-    self.damage = damage
-  def sword(self):
-    print(f"Your sword deals a damage of {self.damage}")
-    print(f"Your sword has a durability rating of {self.durability}")
-
-sword_specs = Weapons(10, 8)
-sword_specs.sword()
+bow = Bow(10, 10)
+bow.flamming_arrow()
