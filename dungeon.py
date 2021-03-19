9a1056087fb813711303a0ed97ba8729517e7dcc
carlo carbonell <carlo1678@hotmail.com>
2021-03-19 14:40:00 -0400

    committing changes

---
+++ dungeon.py
@@ -0,0 +1,21 @@
+# Make a dungeon and make a new enemy inside the dungeon, make a new function called dungeon (after the initial function) make a character class called dragon and give him stats, 
+# put the dragon inside the dungeon
+
+
+
+def dungeon():
+    print("""Welcome to the Dungeon!!
+             You've made it this far, 
+             but be careful, danger 
+             lurks around every corner!""")
+    enemy1 = Warlock()
+    print(f"{enemy1.hp}")
+ 
+    
+
+def underWaterLevel():
+    print("""You are now underwater...
+             How does it feel?""")
+
+
+dungeon()
