c81ba452a5d4ba9c04b7e995fe0e753ff9c09b0d
Tatien Miller <tatmil99@gmail.com>
2021-03-18 23:21:16 -0500

    class inherits from hero class and has working functions

--- evil_char_class.py
+++ evil_char_class.py
@@ -1,29 +1,42 @@
+from charClasses import Warrior
 
-class Sword:
-  def __init__(self, damage, durability):
-    self.damage = damage
-    self.durability = durability
-    # self.hp += 5
+class Sword(Warrior):
+  def __init__(self):
+    self.durability = 15
+    self.damage = 18
+    super().__init__()
+  def attributes(self):
+    self.strength += 20
+    print(f"{self.strength}")
+    self.defense += 10
+    print(f"{self.defense}")
+  def attack(self):
+    self.damage = 18
+    self.durability -= 5
+  def constitution(self):
+    if self.durability <= 0:
+      print("Your sword has broken")
 
-  def short_sword(self):
-    self.damage = damage
-    self.durability = durability
-  def long_sword(self, damage, durability):
-    pass
+sword = Sword()
+print(f"sword durability is {sword.durability}")
+print(f"sword damage is {sword.damage}")
+
+sword.attributes()
+  
       
-class Bow:
-  def __init__(self, damage, durability):
-    self.damage = damage
-    self.durability = durability
-  def flamming_arrow(self):
-    self.damage += 5
-    print(f"{self.damage}")
-    self.durability += 5
-    print(f"{self.durability}")
+# class Bow:
+#   def __init__(self, damage, durability):
+#     self.damage = damage
+#     self.durability = durability
+#   def flamming_arrow(self):
+#     self.damage += 5
+#     print(f"{self.damage}")
+#     self.durability += 5
+#     print(f"{self.durability}")
     
 
-sword = Sword(21, 19)
-print(f"Your short sword damage and durability are {sword.damage} {sword.durability} respectively")
+# sword = Sword(21, 19)
+# print(f"Your short sword damage and durability are {sword.damage} {sword.durability} respectively")
 
-bow = Bow(10, 10)
-bow.flamming_arrow()
+# bow = Bow(10, 10)
+# bow.flamming_arrow()
