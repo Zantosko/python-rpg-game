91510a29abaaa1902128056d38e8757a080322e7
Randi Reyna <77865204+randi09@users.noreply.github.com>
2021-03-19 13:39:13 -0500

    made correction

--- RPGgame.py
+++ RPGgame.py
@@ -69,30 +69,6 @@ potion = PotionShop()
 potion.add_to_inventory()
 
    
- 
-
- 
-
-# Define New Character function
-# def newcharacter():
-#     entry1 = input("What is your name?")
-#     entry3 = input("Please select one of the following. (1) Hero, (2) Knight, (3) Mage ")
-
-#     userInput = entry3
-
-#     if userInput == "1":
-#         character = Hero(10, 10, 50)
-#         print("You have selected to be a Hero")
-#     elif userInput == "2":
-#         character = Hero(10, 10, 50)
-#         print("You have selected to be a Knight")
-#     elif userInput == "3":
-#         character = Hero(10, 10, 50)
-#         print("You have selected to be a Mage")
-#     else:
-#         pass
-
-# newcharacter()
 
 # Characters
 
